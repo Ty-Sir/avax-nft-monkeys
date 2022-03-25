4e3694024d55771ccfdfa266c957dde1cd504f75
Tyler Siravo <tylersiravo@gmail.com>
2022-03-25 13:49:52 -0400

    contract params

--- migrations/2_monkey_migration.js
+++ migrations/2_monkey_migration.js
@@ -1,7 +1,7 @@
 const Monkeys = artifacts.require("Monkeys");
 
 const mintPrice = 444000000000000000;
-const baseUri = "";//your baseUri for your metadata
+const baseUri = "https://ipfs.moralis.io:2053/ipfs/QmRzsuNrQm6GAE5BmtbpkbGFftgwvShTExCBsoHzKubHP7/metadata/";//your baseUri for your metadata
 const uriExt = ".json"; 
 
 module.exports = function (deployer) {
--- migrations/3_marketplace_migration.js
+++ migrations/3_marketplace_migration.js
@@ -1,5 +1,8 @@
 const Marketplace = artifacts.require("Marketplace");
 
+const publisherWallet = 0x9cbA6C997fAc9d0f600a8e0CCB20473c0c4bE9f1;
+const feeAmount = 5;
+
 module.exports = function (deployer) {
-  deployer.deploy(Marketplace);
+  deployer.deploy(Marketplace, publisherWallet, feeAmount);
 };
